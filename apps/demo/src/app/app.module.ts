7199f584000e388594efc7948f420152b25ffb48
Sergio Huanca <sergio.huanca@eoschools.org>
2024-11-11 23:20:12 -0500

    removed unused module imports

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { MazeModule } from './features/maze/maze.module';
@@ -16,8 +14,6 @@ import { API_CLIENT_PROVIDERS } from './core/services/api-client.provider';
   imports: [
     BrowserModule,
     HttpClientModule,
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
     MazeModule
   ],
   providers: [
